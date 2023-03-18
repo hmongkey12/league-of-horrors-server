71fc2987dfabe833bffd9a02411ca08c4d7b48e8
hmongkey12 <khammadaolee@gmail.com>
2023-03-18 14:54:59 -0700

    [test] Updated tests to NetworkHandler, created close socket method

--- src/test/java/com/league/udpserver/handlers/NetworkHandlerTest.java
+++ src/test/java/com/league/udpserver/handlers/NetworkHandlerTest.java
@@ -1,10 +1,10 @@
 package com.league.udpserver.handlers;
 
 import com.serializers.SerializableGameState;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -14,8 +14,11 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(InputHandler.class)
 public class NetworkHandlerTest {
     private static final String SERVER_IP_ADDRESS = "127.0.0.1";
     private static final int CLIENT_PORT = 8085;
@@ -35,7 +38,18 @@ public class NetworkHandlerTest {
         clientSocket = new DatagramSocket(CLIENT_PORT);
     }
 
+    @After
+    public void tearDown() {
+        if (clientSocket != null) {
+            clientSocket.close();
+        }
+        if (networkHandler != null) {
+            networkHandler.closeServerSocket();
+        }
+    }
+
     @Test
+    @PrepareForTest(InputHandler.class)
     public void inputHandler_shouldBeCalled_whenClientSendsCommandPacket() throws IOException {
         // Setup
         PowerMockito.mockStatic(InputHandler.class);
@@ -67,7 +81,44 @@ public class NetworkHandlerTest {
         listeningThread.interrupt();
 
         // Verify: check if InputHandler.handleInput is called with the correct args
-        PowerMockito.verifyStatic(InputHandler.class, Mockito.times(1));
-        InputHandler.handleInput(Mockito.any(SerializableGameState.class), Mockito.eq(new String[]{"left", "playerid"}));
+        PowerMockito.verifyStatic(InputHandler.class, times(1));
+        InputHandler.handleInput(any(SerializableGameState.class), eq(new String[]{"left", "playerid"}));
+    }
+
+    @Test
+    @PrepareForTest(CreationHandler.class)
+    public void creationHandler_shouldBeCalled_whenClientSendsCreateHeroPacket() throws IOException {
+        // Setup
+        PowerMockito.mockStatic(CreationHandler.class);
+        String createHeroMessage = "{\"createHero\":\"heroName_playerid\"}";
+        DatagramPacket outgoingPacket = new DatagramPacket(createHeroMessage.getBytes(), createHeroMessage.length(), serverIpAddress, SERVER_PORT);
+
+        // Start listening for the response
+        Thread listeningThread = new Thread(() -> networkHandler.listen());
+        listeningThread.start();
+
+        // Wait for the server to start listening
+        try {
+            Thread.sleep(WAIT_TIME_BEFORE_SEND);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Trigger: send the createHero command to the server
+        clientSocket.send(outgoingPacket);
+
+        // Wait for the server to process the command
+        try {
+            Thread.sleep(WAIT_TIME_AFTER_SEND);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Stop the listening thread
+        listeningThread.interrupt();
+
+        // Verify: check if CreationHandler.handleCreation is called with the correct args
+        PowerMockito.verifyStatic(CreationHandler.class, times(1));
+        CreationHandler.handleCreation(any(SerializableGameState.class), eq("playerid"), eq("heroName"));
     }
 }
