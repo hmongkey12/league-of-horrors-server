d95645ca2e748c46a01e36019849c778258465a1
hmongkey12 <khammadaolee@gmail.com>
2023-03-31 11:35:01 -0700

    [refactor] refactored InputHandler method

--- src/main/java/com/league/udpserver/handlers/InputHandler.java
+++ src/main/java/com/league/udpserver/handlers/InputHandler.java
@@ -25,39 +25,75 @@ public class InputHandler {
         }
         switch (command) {
             case "left":
-                playerEntity.setFacingDirection("left");
-                int leftXPos = playerEntity.getXPos() - MOVESPEED;
-                playerEntity.setXPos(leftXPos < BOUNDARY_LEFT ? BOUNDARY_LEFT : leftXPos);
-                playerEntity.setMoving(true);
-                playerEntity.setMovingStart(System.nanoTime());
+                movePlayerLeft(playerEntity);
                 break;
             case "right":
-                playerEntity.setFacingDirection("right");
-                int rightXPos = playerEntity.getXPos() + MOVESPEED;
-                playerEntity.setXPos(rightXPos > BOUNDARY_RIGHT ? BOUNDARY_RIGHT : rightXPos);
-                playerEntity.setMoving(true);
-                playerEntity.setMovingStart(System.nanoTime());
+                movePlayerRight(playerEntity);
                 break;
             case "up":
-                if (!playerEntity.isJumping() && !playerEntity.isFalling()) {
-                    playerEntity.setJumping(true);
-                }
+                initiateJump(playerEntity);
                 break;
             case "skill1":
-                if (!playerEntity.isAttacking()) {
-                    int yPos = playerEntity.getYPos();
-                    int xPos = playerEntity.getXPos();
-                    playerEntity.setAttacking(true);
-                    playerEntity.setMoving(false);
-                    playerEntity.getAbilities().get(0).setYPos(yPos);
-                    playerEntity.setAttackStart(System.nanoTime());
-                    if (playerEntity.getFacingDirection().equals("left")) {
-                        playerEntity.getAbilities().get(0).setXPos(xPos - 200);
-                    } else if (playerEntity.getFacingDirection().equals("right")) {
-                        playerEntity.getAbilities().get(0).setXPos(xPos + 200);
-                    }
-                }
+                useSkill1(playerEntity);
                 break;
+        }
+    }
+
+    /**
+     * Moves the player to the left, updating the player's position and facing direction.
+     * The player's position will not go beyond the left boundary.
+     * @param playerEntity represents the shared serializable player state object that can be passed between server and client.
+     */
+    private static void movePlayerLeft(SerializableHeroEntity playerEntity) {
+        playerEntity.setFacingDirection("left");
+        int newXPos = playerEntity.getXPos() - MOVESPEED;
+        playerEntity.setXPos(Math.max(BOUNDARY_LEFT, newXPos));
+        playerEntity.setMoving(true);
+        playerEntity.setMovingStart(System.nanoTime());
+    }
+
+    /**
+     * Moves the player to the right, updating the player's position and facing direction.
+     * The player's position will not go beyond the right boundary.
+     * @param playerEntity represents the shared serializable player state object that can be passed between server and client.
+     */
+    private static void movePlayerRight(SerializableHeroEntity playerEntity) {
+        playerEntity.setFacingDirection("right");
+        int newXPos = playerEntity.getXPos() + MOVESPEED;
+        playerEntity.setXPos(Math.min(BOUNDARY_RIGHT, newXPos));
+        playerEntity.setMoving(true);
+        playerEntity.setMovingStart(System.nanoTime());
+    }
+
+    /**
+     * Initiates a jump for the player if they are not already jumping or falling.
+     * @param playerEntity represents the shared serializable player state object that can be passed between server and client.
+     */
+    private static void initiateJump(SerializableHeroEntity playerEntity) {
+        if (!playerEntity.isJumping() && !playerEntity.isFalling()) {
+            playerEntity.setJumping(true);
+        }
+    }
+
+    /**
+     * Executes the player's first skill, updating their attacking state and setting the position of the ability.
+     * The skill will only be executed if the player is not already attacking.
+     * @param playerEntity represents the shared serializable player state object that can be passed between server and client.
+     */
+    private static void useSkill1(SerializableHeroEntity playerEntity) {
+        if (!playerEntity.isAttacking()) {
+            int yPos = playerEntity.getYPos();
+            int xPos = playerEntity.getXPos();
+            playerEntity.setAttacking(true);
+            playerEntity.setMoving(false);
+            playerEntity.getAbilities().get(0).setYPos(yPos);
+            playerEntity.setAttackStart(System.nanoTime());
+
+            if (playerEntity.getFacingDirection().equals("left")) {
+                playerEntity.getAbilities().get(0).setXPos(xPos - 200);
+            } else if (playerEntity.getFacingDirection().equals("right")) {
+                playerEntity.getAbilities().get(0).setXPos(xPos + 200);
+            }
         }
     }
 }
