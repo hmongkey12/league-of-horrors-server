856621eb290beb81377b60c332933a9c909d9436
hmongkey12 <khammadaolee@gmail.com>
2023-04-03 11:15:54 -0700

    [documentation] added java doc comments to NetworkHandler

--- src/main/java/com/league/udpserver/handlers/NetworkHandler.java
+++ src/main/java/com/league/udpserver/handlers/NetworkHandler.java
@@ -47,6 +47,11 @@ public class NetworkHandler {
 
     private ApplicationContext applicationContext;
     private JSONParser jsonParser;
+
+    /**
+     * NetworkHandler constructor initializes the game state, connected players, and sets up necessary socket communication.
+     * @param gameState the current state of the game represented as a SerializableGameState object.
+     */
     public NetworkHandler(SerializableGameState gameState) {
         this.gameState = gameState;
         connectedPlayers = new ConcurrentHashMap<>();
@@ -62,6 +67,9 @@ public class NetworkHandler {
         }
     }
 
+    /**
+     * Listens for incoming packets, schedules the heartbeat task, and processes the received data.
+     */
     public void listen() {
         scheduleHeartbeatTask();
         while (true) {
@@ -88,6 +96,10 @@ public class NetworkHandler {
         }
     }
 
+    /**
+     * Removes disconnected players from the game state based on their last received heartbeat timestamp.
+     * @param gameState the current state of the game represented as a SerializableGameState object.
+     */
     private void useHeartBeatToRemoveDisconnectedPlayers(SerializableGameState gameState) {
         if (connectedPlayers != null) {
             ConcurrentHashMap<String, Long> connectedPlayersCopy = new ConcurrentHashMap<>(connectedPlayers);
@@ -101,32 +113,54 @@ public class NetworkHandler {
             });
         }
     }
+
+    /**
+     * Closes the server socket to stop listening for incoming packets.
+     */
     public void closeServerSocket() {
         if (serverSocket != null) {
             serverSocket.close();
         }
     }
 
+    /**
+     * Schedules the heartbeat task to run periodically and remove disconnected players.
+     */
     private void scheduleHeartbeatTask() {
         heartbeatExecutor.scheduleAtFixedRate(() -> useHeartBeatToRemoveDisconnectedPlayers(gameState),
                 0, TimeUnit.SECONDS.toMillis((long) TIME_THRESHOLD_SECONDS), TimeUnit.MILLISECONDS);
     }
 
+    /**
+     * Receives the incoming datagram packet from clients.
+     * @throws IOException if there is an error receiving the datagram packet.
+     */
     private void receiveIncomingPacket() throws IOException {
         serverSocket.receive(incomingDatagramPacket);
     }
 
+    /**
+     * Parses the incoming packet and stores the result in a mappedJsonString object.
+     * @throws ParseException if there is an error parsing the JSON string.
+     */
     private void parseIncomingPacket() throws ParseException {
         String incomingString = new String(incomingDatagramPacket.getData(), 0, incomingDatagramPacket.getLength());
         jsonParser = new JSONParser(incomingString);
         mappedJsonString = (Map<String, String>) jsonParser.parse();
     }
 
+    /**
+     * Handles the command received in the packet and calls the appropriate method based on the command type.
+     */
     private void handleCommand() {
         String[] args = mappedJsonString.get("command").split("_");
         InputHandler.handleInput(gameState, args);
     }
 
+    /**
+     * Handles the createHero command, creates a new hero for the player, and sends the updated game state.
+     * @throws IOException if there is an error sending the updated game state.
+     */
     private void handleCreateHero() throws IOException {
         String[] args = mappedJsonString.get("createHero").split("_");
         String playerId = args[1];
@@ -136,16 +170,28 @@ public class NetworkHandler {
         sendSerializedGameState();
     }
 
+    /**
+     * Checks if the received packet is a getUpdate command and if the player is connected.
+     * @return true if it's a getUpdate command and the player is connected, false otherwise.
+     */
     private boolean isGetUpdate() {
         return mappedJsonString.containsKey("getUpdate") && connectedPlayers.containsKey(mappedJsonString.get("getUpdate"));
     }
 
+    /**
+     * Handles the getUpdate command, updates the game state, and sends the updated game state to the client.
+     * @throws IOException if there is an error sending the updated game state.
+     */
     private void handleGetUpdate() throws IOException {
         UpdateHandler.handleUpdates(gameState, mappedJsonString.get("getUpdate"));
         connectedPlayers.put(mappedJsonString.get("getUpdate"), System.currentTimeMillis());
         sendSerializedGameState();
     }
 
+    /**
+     * Sends the serialized game state to the client.
+     * @throws IOException if there is an error sending the serialized game state.
+     */
     private void sendSerializedGameState() throws IOException {
         SerializableGameStateDecorator serializableGameStateDecorator = new SerializableGameStateDecorator(new BasicSerializer());
         outgoingDatagramPacketBuffer = serializableGameStateDecorator.serialize(gameState);
